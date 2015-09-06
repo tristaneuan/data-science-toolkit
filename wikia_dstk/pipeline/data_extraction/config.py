ccbe29d48b628284b05c1cc4f593fb42dd227e52
Tristan Chong <tristanchong@gmail.com>
2015-09-06 15:40:35 -0700

    Update AMI to dstk 2015-09-06a

--- wikia_dstk/pipeline/data_extraction/config.py
+++ wikia_dstk/pipeline/data_extraction/config.py
@@ -2,7 +2,7 @@ default_config = {
     "queue": "data_events",
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-6dd9ca5d",  # dstk 2015-09-02
+    "ami": "ami-0bfdef3b",  # dstk 2015-09-06a
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",
--- wikia_dstk/pipeline/wiki_data_extraction/config.py
+++ wikia_dstk/pipeline/wiki_data_extraction/config.py
@@ -1,7 +1,7 @@
 config = {
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-6dd9ca5d",  # dstk 2015-09-02
+    "ami": "ami-0bfdef3b",  # dstk 2015-09-06a
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",
